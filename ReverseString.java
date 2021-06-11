25faf4b0277156b3f1e81c670c0820befb47eeb8
Matthew Freeman <matthew.freeman821@gmail.com>
2021-06-11 11:37:42 -0400

    Adds file to gitignore and updates reverstring method

--- ReverseString.java
+++ ReverseString.java
@@ -1,12 +1,11 @@
 public class ReverseString {
   public static String reverseString(String s) {
-    String reversed = "";
+    String reversedString = "";
     char[] stringArray = s.toCharArray();
-
     for (int i = s.length() - 1; i >= 0; i--) {
-      reversed = reversed + stringArray[i];
+      reversedString = reversedString + stringArray[i];
     }
-    return reversed;
+    return reversedString;
   }
 
   public static void main(String[] args) {
