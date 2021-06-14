1adc0fbaff95baf896348e56f41ad232ca3c41a5
Matthew Freeman <matthew.freeman821@gmail.com>
2021-06-14 14:58:03 -0400

    Fixes the second reverse method by removing the += and making it just =

--- ReverseString.java
+++ ReverseString.java
@@ -11,10 +11,7 @@ public class ReverseString {
   public static String reverseString2(String s) {
     String reversedString2 = "";
     for (int i = s.length() - 1; i >= 0; i--) {
-      System.out.println(i);
-      System.out.println(s.charAt(i));
-      reversedString2 += reversedString2 + s.charAt(i);
-      System.out.println(reversedString2.length());
+      reversedString2 = reversedString2 + s.charAt(i);
     }
     return reversedString2;
   }
@@ -23,6 +20,6 @@ public class ReverseString {
     System.out.println("Reversing a string:");
     System.out.println(reverseString("Well hello there"));
     System.out.println("Second way to reverse a string:");
-    System.out.println(reverseString2("General"));
+    System.out.println(reverseString2("General Kenobi"));
   }
 }
