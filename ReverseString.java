db8d66907779f8bd9aa91fbc43fb4a255dc68273
Matthew Freeman <matthew.freeman821@gmail.com>
2021-06-27 09:08:28 -0400

    Updates to evaluate correct count

--- ReverseString.java
+++ ReverseString.java
@@ -32,7 +32,7 @@ public class ReverseString {
         count++;
       }
     }
-    return count == s.length();
+    return count == s.length() / 2;
   }
 
   public static void main(String[] args) {
