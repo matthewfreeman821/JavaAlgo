da62fe7ff790e3116b2375ef086f6de2a6611b70
Matthew Freeman <matthew.freeman821@gmail.com>
2021-07-06 09:14:14 -0400

    Fixes error by adding in correct number of parenthesis

--- ReverseString.java
+++ ReverseString.java
@@ -49,8 +49,8 @@ public class ReverseString {
   public static void main(String[] args) {
     String pali = "Hello olleH";
     String notPali = "Hello";
-    System.out.println(isPalindrome(pali);
-    System.out.println(isPalindrome2(pali);
+    System.out.println(isPalindrome(pali));
+    System.out.println(isPalindrome2(pali));
 
   }
 }
